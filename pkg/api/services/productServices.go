f7f313edf1c922ff8627c9da1c740ef2638669d5
fazilnbr <fazilkp2000@gmail.com>
2023-04-14 16:32:11 +0530

    getProduct service fuction
    here defined the getProduct service fuction and connect with usecase then return the protobuf response

--- pkg/api/services/productServices.go
+++ pkg/api/services/productServices.go
@@ -13,28 +13,47 @@ type ProductService struct {
 	userUseCase usecase.ProductUseCase
 }
 
-func (p *ProductService) CreateProduct(ctx context.Context, req *pb.CreateProductRequest) (*pb.CreateProductResponse, error) {
-	product:=domain.Product{
-		Id: req.Id,
-		Name: req.Name,
-		Description: req.Description,
-		Price: float64(req.Price),
-		Stock: req.Stock,
+func (p *ProductService) GetProduct(ctx context.Context, req *pb.GetProductRequest) (*pb.GetProductResponse, error) {
+	product, err := p.userUseCase.GetProduct(ctx, req.Id)
+	if err != nil {
+		return &pb.GetProductResponse{
+			Status: http.StatusUnprocessableEntity,
+			Error:  err.Error(),
+		}, err
 	}
-	id,err:=p.userUseCase.CreateProduct(ctx,product)
 
-	if err!=nil{
+	return &pb.GetProductResponse{
+		Status:      http.StatusOK,
+		Id:          product.Id,
+		Name:        product.Name,
+		Description: product.Description,
+		Price:       float32(product.Price),
+		Stock:       product.Stock,
+	}, nil
+
+}
+
+func (p *ProductService) CreateProduct(ctx context.Context, req *pb.CreateProductRequest) (*pb.CreateProductResponse, error) {
+	product := domain.Product{
+		Id:          req.Id,
+		Name:        req.Name,
+		Description: req.Description,
+		Price:       float64(req.Price),
+		Stock:       req.Stock,
+	}
+	id, err := p.userUseCase.CreateProduct(ctx, product)
+
+	if err != nil {
 		return &pb.CreateProductResponse{
 			Status: http.StatusUnprocessableEntity,
-			Error: err.Error(),
-		},err
+			Error:  err.Error(),
+		}, err
 	}
 
 	return &pb.CreateProductResponse{
 		Status: http.StatusOK,
-		Id: id,
-	},nil
-
+		Id:     id,
+	}, nil
 
 }
 func NewProductService(usecase usecase.ProductUseCase) *ProductService {
