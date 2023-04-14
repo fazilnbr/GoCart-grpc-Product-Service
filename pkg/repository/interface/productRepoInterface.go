0cf49ec0b94a3a45681fbbd2adea1df05335855a
fazilnbr <fazilkp2000@gmail.com>
2023-04-14 15:25:32 +0530

    product repoctory
    defined the product repocitory function

--- pkg/repository/interface/productRepoInterface.go
+++ pkg/repository/interface/productRepoInterface.go
@@ -1,5 +1,11 @@
 package interfaces
 
+import (
+	"context"
+
+	"github.com/fazilnbr/GoCart-grpc-Product-Service/pkg/domain"
+)
+
 type ProductRepository interface {
-	// CreateProduct(ctx context.Context, user domain.Product) (int, error)
+	CreateProduct(ctx context.Context, product domain.Product) (int64, error)
 }
--- pkg/repository/productRepository.go
+++ pkg/repository/productRepository.go
@@ -1,6 +1,9 @@
 package repository
 
 import (
+	"context"
+
+	"github.com/fazilnbr/GoCart-grpc-Product-Service/pkg/domain"
 	interfaces "github.com/fazilnbr/GoCart-grpc-Product-Service/pkg/repository/interface"
 	"gorm.io/gorm"
 )
@@ -9,11 +12,14 @@ type productDatabase struct {
 	DB *gorm.DB
 }
 
-// // CreateProduct implements interfaces.UserRepository
-// func (*userDatabase) CreateProduct(ctx context.Context, user domain.Product) (int, error) {
-
-// }
+// CreateProduct implements interfaces.ProductRepository
+func (c *productDatabase) CreateProduct(ctx context.Context, product domain.Product) (int64, error) {
+	err := c.DB.Create(&product).Error
+	return product.Id, err
+}
 
 func NewProductDatabase(DB *gorm.DB) interfaces.ProductRepository {
-	return &productDatabase{DB}
+	return &productDatabase{
+		DB,
+	}
 }
